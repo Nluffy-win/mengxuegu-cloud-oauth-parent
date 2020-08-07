daa1141efa03182d9dc475c4dc3165347588fe85
赵杰 <345227647@qq.com>
2020-08-07 09:12:12 +0800

    调整运行，正常启动

---
+++ mengxuegu-cloud-oauth-server/src/main/java/com/mengxuegu/oauth2/server/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.mengxuegu.oauth2.server.config;
+
+
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+///**
+// * @ClassName: MybatisPlusConfig
+// * @Description: mybatis-plus 配置 $
+// * @author: NieSC
+// * @Date: 2020-07-13 16:08
+// */
+//@EnableTransactionManagement
+//@MapperScan("com.gdyx.cloud.user.dao")
+//@Configuration
+//public class MybatisPlusConfig {
+//
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor(){
+//        return new PaginationInterceptor();
+//    }
+//}
