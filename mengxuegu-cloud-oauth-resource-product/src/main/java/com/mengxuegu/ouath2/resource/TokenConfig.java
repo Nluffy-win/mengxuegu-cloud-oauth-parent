fe0878f7a94f7764d9c3b4bd2792c0ca9478a841
赵杰 <345227647@qq.com>
2020-08-11 09:46:27 +0800

    私钥加密，公钥解密，非对称加密

--- mengxuegu-cloud-oauth-resource-product/src/main/java/com/mengxuegu/ouath2/resource/TokenConfig.java
+++ mengxuegu-cloud-oauth-resource-product/src/main/java/com/mengxuegu/ouath2/resource/TokenConfig.java
@@ -1,10 +1,14 @@
 package com.mengxuegu.ouath2.resource;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.IOException;
 
 /**
  * Created by Y_Coffee on 2020/7/27
@@ -19,7 +23,15 @@ public class TokenConfig {
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
         //采取对称加密进行令牌签名，资源服务器也要此秘钥解密，校验令牌合法性
-        converter.setSigningKey(SIGNING_KEY);
+        //converter.setSigningKey(SIGNING_KEY);
+        ClassPathResource resource = new ClassPathResource("public.txt");
+        String publicKey = null;
+        try {
+            publicKey = IOUtils.toString(resource.getInputStream(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        converter.setVerifierKey(publicKey);
         return converter;
     }
 
