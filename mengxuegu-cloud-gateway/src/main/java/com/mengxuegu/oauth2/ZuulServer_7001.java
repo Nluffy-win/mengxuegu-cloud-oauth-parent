326c4a6bdff8d7052202aba2d177f93b307749e7
赵杰 <345227647@qq.com>
2020-08-12 16:15:57 +0800

    配置网关

--- mengxuegu-cloud-gateway/src/main/java/com/mengxuegu/oauth2/ZuulServer_7001.java
+++ mengxuegu-cloud-gateway/src/main/java/com/mengxuegu/oauth2/ZuulServer_7001.java
@@ -1,18 +1,19 @@
-package com.mengxuegu.ouath2;
+package com.mengxuegu.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * 资源服务
- * @author CoffeeY
- * @Classname ProductResourceApplication * @Description TODO * @Date 2020/8/9 21:49 * @Created by John
+ * Created by Y_Coffee on 2020/8/12
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class ProductResourceApplication {
+@EnableZuulProxy //开启zuul的功能
+public class ZuulServer_7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(ProductResourceApplication.class, args);
+        SpringApplication.run(ZuulServer_7001.class, args);
     }
 }
